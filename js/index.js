3a924225345bb14d35aaa6eefbcdc2cdf8a4c1bc
Abdulbas1d <muhammadaliyevabdulbosid@gmail.com>
2024-10-22 13:46:39 +0500

    Open files

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//////////----- Massivlarga doir misollar -----//////////
+
+
+
+// 1-Misol
+
+// Berilgan massivdagi so'zlardan uzunligi 5 ta belgidan kam bo'lganlarni olib tashlab, qolgan so'zlarning har birining bosh harfini katta harfga aylantiruvchi funksiyani yozing.
+
+// let arr = ["salom", "true", 25, false, "hayrli tong"];
+// let res = arr.filter(function(value) {
+//     return value.length >= 5;
+// })
+
+// let res1 = res.map(function(value) {
+//     return value.charAt(0).toUpperCase() + value.slice(1);
+// })
+// console.log(res1);
+
+
+
+
+// 2-Misol
+
+// Talabalarning ism va yoshlarini o'z ichiga olgan massivdan 20 yoshdan katta bo'lgan birinchi talabani topuvchi va uning ismini katta harf bilan qaytaruvchi funksiyani yozing.
+
+let arr = [];
+
+
